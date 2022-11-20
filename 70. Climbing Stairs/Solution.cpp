3452cb39c3900e5e4e0d53c6030b02c7a840be1a
danishrp102 <75444712+danishrp102@users.noreply.github.com>
2022-11-20 13:21:56 +0530

    Create Solution.cpp

---
+++ 70. Climbing Stairs/Solution.cpp
@@ -0,0 +1,41 @@
+// Problem link: https://leetcode.com/problems/climbing-stairs/description/
+
+// Memoized Recursive code:
+
+class Solution {
+public:
+
+    int solve(int n, vector <int> &dp) {
+        if(n == 0)
+            return 1;
+        if(n < 0)
+            return 0;
+        if(dp[n] != -1)
+            return dp[n];
+        
+        return dp[n] = solve(n - 1, dp) + solve(n - 2, dp);
+    }
+
+    int climbStairs(int n) {
+        vector <int> dp(n + 1, -1);
+        return solve(n, dp);
+    }
+};
+
+// Tabular code:
+
+class Solution {
+public:
+
+    int climbStairs(int n) {
+        if(n <= 2)
+            return n;
+
+        int a = 1, b = 2, c;
+
+        for(int i = 3; i <= n; i++)
+            c = a + b, a = b, b = c;
+        
+        return c;
+    }
+};
