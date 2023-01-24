5c4cf85d5bcb6375730ec8422bb0bf9dde585fb5
Danish Pinto <75444712+danishrp102@users.noreply.github.com>
2023-01-25 00:16:51 +0530

    Create Solution.cpp

---
+++ 2001. Number of Pairs of Interchangeable Rectangles/Solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    long long interchangeableRectangles(vector<vector<int>>& rectangles) {
+        int n = rectangles.size();
+        map <double, long long> mp;
+
+        for(int i = 0; i < n; i++) {
+            double val = (double)rectangles[i][0] / (double)rectangles[i][1];
+            mp[val]++;
+        }
+
+        long long ans = 0;
+        for(auto [x, y]: mp) {
+            ans += y * (y - 1) / 2;
+        }
+
+        return ans;
+    }
+};
