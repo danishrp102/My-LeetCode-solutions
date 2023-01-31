9f5c0e53c7347713c7fb24bd49cb5f752d94cdb9
Danish Pinto <75444712+danishrp102@users.noreply.github.com>
2023-01-31 23:38:53 +0530

    Create Solution.cpp

---
+++ 1626. Best Team With No Conflicts/Solution.cpp
@@ -0,0 +1,31 @@
+// Problem link: https://leetcode.com/problems/best-team-with-no-conflicts/description/
+//  Variation of the Longest Increasing Subsequence Problem
+
+class Solution {
+public:
+
+    int bestTeamScore(vector<int>& scores, vector<int>& ages) {
+        int n = ages.size();
+        vector <pair <int, int> > v(n);
+
+        for(int i = 0; i < n; i++) {
+            v[i].first = ages[i];
+            v[i].second = scores[i];
+        } 
+
+        sort(v.begin(), v.end());
+        vector <int> dp(n);
+
+        for(int i = 0; i < n; i++)
+            dp[i] = v[i].second;
+
+        for(int i = 1; i < n; i++) {
+            for(int j = 0; j < i; j++) {
+                if(v[i].second >= v[j].second)
+                    dp[i] = max(dp[i], dp[j] + v[i].second);
+            }
+        }
+
+        return *max_element(dp.begin(), dp.end());
+    }
+};
