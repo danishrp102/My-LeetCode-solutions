56dc2bf482ab01affbcfaaa49b69b5493c310289
Danish Pinto <75444712+danishrp102@users.noreply.github.com>
2023-04-06 02:00:04 +0530

    Update Solution.cpp

--- 2608. Shortest Cycle in a Graph/Solution.cpp
+++ 2608. Shortest Cycle in a Graph/Solution.cpp
@@ -3,20 +3,27 @@
 class Solution {
 public:
 
-    void solve(vector <vector <int> > &adj, vector <int> &time, int t, int node, int par, int &ans) {
-        if(time[node] != -1) {
-            if(t - time[node] > 0)
-                ans = min(ans, t - time[node]);
-            return;
-        }
+    void solve(vector <vector <int> > &adj, int i, vector <int> &dist, vector <int> &par, int &ans) {
+        queue <int> q;
+        q.push(i);
+        dist[i] = 0;
 
-        time[node] = t;
+        while(!q.empty()) {
 
-        for(auto x: adj[node]) {
-            if(x == par)
-                continue;
+            int node = q.front();
+            q.pop();
             
-            solve(adj, time, t + 1, x, node, ans);
+            for(auto x: adj[node]) {
+                
+                if(dist[x] == INT_MAX) {
+                    dist[x] = 1 + dist[node];
+                    q.push(x);
+                    par[x] = node;
+                }
+
+                else if(par[x] != node && par[node] != x)
+                    ans = min(ans, dist[x] + dist[node] + 1);
+            }
         }
     }
 
@@ -30,9 +37,9 @@ public:
 
         int ans = INT_MAX;
 
-        for(int i = 0; i < n; i++) {    
-            vector <int> time(n, -1);
-            solve(adj, time, 0, i, -1, ans);
+        for(int i = 0; i < n; i++) {  
+            vector <int> dist(n, INT_MAX), par(n, -1);
+            solve(adj, i, dist, par, ans);
         }
 
         return (ans == INT_MAX) ? -1 : ans;
