6bce7a342eb869cb5d01429c4e95ac823158d6f4
Danish Pinto <75444712+danishrp102@users.noreply.github.com>
2023-04-07 10:28:38 +0530

    Update Solution.cpp

--- 1020. Number of Enclaves/Solution.cpp
+++ 1020. Number of Enclaves/Solution.cpp
@@ -6,9 +6,9 @@ public:
     int dx[4] = {-1, 1, 0, 0};
     int dy[4] = {0, 0, -1, 1};
 
-    int solve(int i, int j, vector <vector <bool> > &vis, vector <vector <int> > &grid, bool &flag) {
-        vis[i][j] = true;
-        int cnt = 0, m = grid.size(), n = grid[0].size();
+    int solve(int i, int j, vector <vector <int> > &grid, bool &flag, int &m, int &n) {
+        grid[i][j] = 2;
+        int cnt = 0;
 
         if(min(i, j) == 0 || (i == m - 1) || (j == n - 1))
             flag = true;
@@ -16,9 +16,8 @@ public:
         for(int k = 0; k < 4; k++) {
             int nx = i + dx[k], ny = j + dy[k];
 
-            if(min(nx, ny) >= 0 && nx < m && ny < n && grid[nx][ny] == 1 && !vis[nx][ny]) {
-                cnt += solve(nx, ny, vis, grid, flag);
-            }
+            if(min(nx, ny) >= 0 && nx < m && ny < n && grid[nx][ny] == 1)
+                cnt += solve(nx, ny, grid, flag, m, n);
         }
 
         return cnt + 1;
@@ -27,14 +26,13 @@ public:
     int numEnclaves(vector<vector<int>>& grid) {
         int m = grid.size(), n = grid[0].size();
         int ans = 0;
-        vector <vector <bool> > vis(m, vector <bool>(n, false));
 
         for(int i = 0; i < m; i++) {
             for(int j = 0; j < n; j++) {
 
-                if(!vis[i][j] && grid[i][j] == 1) {
+                if(grid[i][j] == 1) {
                     bool flag = false;
-                    int cnt = solve(i, j, vis, grid, flag);
+                    int cnt = solve(i, j, grid, flag, m, n);
 
                     if(!flag)
                         ans += cnt;
