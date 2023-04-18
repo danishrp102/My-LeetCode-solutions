8fb559fa79ffdeede2c98e561675cda94d2c4c80
Danish Pinto <75444712+danishrp102@users.noreply.github.com>
2023-04-18 10:13:08 +0530

    Create Solution.cpp

---
+++ 1768. Merge Strings Alternately/Solution.cpp
@@ -0,0 +1,18 @@
+// Problem link: https://leetcode.com/problems/merge-strings-alternately/description/
+
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        int m = word1.size(), n = word2.size();
+        string ans = "";
+
+        for(int i = 0; i < max(m, n); i++) {
+            if(i < m)
+                ans += word1[i];
+            if(i < n)
+                ans += word2[i];
+        }
+
+        return ans;
+    }
+};
